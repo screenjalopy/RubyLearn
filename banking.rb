d6b07083a52cf138f3a2ea08db247f8257eb8d05
screenjalopy <p44935@gmail.com>
2014-08-25 08:36:55 -0700

    Changes

--- banking.rb
+++ banking.rb
@@ -18,16 +18,17 @@ class Bank
 
 	def deposit(customer, amount)
 		indexCustomer=@customers.index(customer)
+
 		if indexCustomer
 			if customer.money<amount
-				puts "Deposit amount #{amount} is greater than #{customer.name} has (#customer.money})!"
+				puts "Deposit amount #{amount} is greater than #{customer.name} available cash of #{customer.money}!"
 			else
 				@customersmoney[indexCustomer]+=amount
 				customer.money-=amount
 				puts "#{customer.name} deposited #{amount} to #{@name}. #{customer.name} has #{customer.money}. #{@customers[indexCustomer].name} acccount has #{@customersmoney[indexCustomer]}."
 			end
 		else
-			puts "#{customer.name} is not a member of the bank!"
+			puts "#{customer.name} is not a member of #{name}!"
 		end
 	end
 
@@ -35,7 +36,7 @@ class Bank
 		indexCustomer=@customers.index(customer)
 		if indexCustomer
 			if amount>@customersmoney[indexCustomer]
-				puts "Withdrawal amount #{amount} is greater than #{customer.name}s balance of @customersmoney[indexCustomer]!"
+				puts "Withdrawal amount #{amount} is greater than #{customer.name}s balance of #{@customersmoney[indexCustomer]}!"
 			else
 				@customersmoney[indexCustomer]-=amount
 				customer.money+=amount
@@ -49,6 +50,7 @@ class Bank
 	def transfer(customer, instanceBank, amount)
 		indexCustomer=@customers.index(customer)
 		indexOtherBankCustomer=instanceBank.customers.index(customer)
+
 		if indexCustomer && indexOtherBankCustomer
 			if amount>@customersmoney[indexCustomer]
 				puts "Withdrawal amount #{amount} is greater than #{customer.name}s balance of @customersmoney[indexCustomer]!"
@@ -60,6 +62,10 @@ class Bank
 		else
 			puts "#{customer.name} is not a member of #{@name} and/or #{instanceBank.name}!"
 		end
+	end
+
+	def total_cash_in_bank
+		"#{@name} has #{customersmoney.reduce(:+)} in the bank."
 	end
 
 end
@@ -80,9 +86,6 @@ chase = Bank.new("JP Morgan Chase") #JP Morgan Chase bank was just created.
 wells_fargo = Bank.new("Wells Fargo") #Wells Fargo bank was just created.
 me = Person.new("Shehzan", 500) #Hi, Shehzan. You have $500!
 friend1 = Person.new("John", 1000) #Hi, John. You have $1000!
-#delete block
-test1=Person.new("what",2002)
-#delete block
 chase.open_account(me) #Shehzan, thanks for opening an account at JP Morgan Chase!
 chase.open_account(friend1) #John, thanks for opening an account at JP Morgan Chase!
 wells_fargo.open_account(me) #Shehzan, thanks for opening an account at Wells Fargo!
@@ -96,13 +99,13 @@ chase.withdraw(me, 50) #Shehzan withdrew $50 from JP Morgan Chase. Shehzan has $
 # Transfers
 chase.transfer(me, wells_fargo, 100) #Shehzan transfered $100 from the JP Morgan Chase account to the Wells Fargo account. The JP Morgan Chase account has $50 and the Wells Fargo account has $100.
 
-# # Extra Credit level 1: Validate
-# chase.deposit(me, 5000) #Shehzan does not have enough cash to deposit $5000.
-# chase.withdraw(me, 5000) #Shehzan does not have enough money in the account to withdraw $5000.
+# Extra Credit level 1: Validate
+chase.deposit(me, 5000) #Shehzan does not have enough cash to deposit $5000.
+chase.withdraw(me, 5000) #Shehzan does not have enough money in the account to withdraw $5000.
 
-# # Extra Credit level 2: Count totals
-# puts chase.total_cash_in_bank #JP Morgan has $350 in the bank.
-# puts wells_fargo.total_cash_in_bank #Wells Fargo has $100 in the bank.
+# Extra Credit level 2: Count totals
+puts chase.total_cash_in_bank #JP Morgan has $350 in the bank.
+puts wells_fargo.total_cash_in_bank #Wells Fargo has $100 in the bank.
 
 # #Extra Credit level 5: Credit Cards
 # #For this open-ended problem, you should create your own test cases and add it to the bottom of your solution
